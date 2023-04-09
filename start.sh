3416f47d024ac74672e208d69714afb9c6ce7991
jgf <fdsefxc@proton.me>
2023-04-09 13:18:01 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 PORT=3000
-AUUID=5aaed9b7-7fe3-47c3-bb52-db59859ce198
-ParameterSSENCYPT=chacha20-ietf-poly1305
-CADDYIndexPage=https://raw.githubusercontent.com/caddyserver/dist/master/welcome/index.html
-CaddyConfig=https://raw.githubusercontent.com/bsefwe/glitch-Xray/main/etc/Caddyfile
-XRayConfig=https://raw.githubusercontent.com/bsefwe/glitch-Xray/main/etc/config.json
-# download execution
+
 chmod +x caddy xray
 
 
